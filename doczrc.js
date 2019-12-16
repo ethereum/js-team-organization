3e493e5439be0c7fd194f93be3ef9279cfc9849a
Marc Garreau <marcdgarreau@gmail.com>
2019-12-16 14:44:23 -0700

    feat: introduces docz site generation (#9)
    * Introduces docz site generation
    
    * Deploys docz to GitHub Pages

---
+++ doczrc.js
@@ -0,0 +1,6 @@
+export default {
+  title: 'EF JavaScript Team',
+  description: 'A guide to projects maintained by the EF JS team',
+  files: './website/**/*.{md,mdx}',
+  base: '/js-organization/'
+}
