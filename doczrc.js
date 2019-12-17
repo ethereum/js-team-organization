76f0aba1eabc108c1caaa64975d5e84ef8031150
Ev <ev@ethereum.org>
2019-12-16 20:25:52 -0500

    docs: fix base paths (#13)
    * Base paths
    
    * variable name

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,5 @@
 export default {
   title: 'EF JavaScript Team',
   description: 'A guide to projects maintained by the EF JS team',
-  files: './website/**/*.{md,mdx}',
-  base: '/js-organization/'
+  files: './website/**/*.{md,mdx}'
 }
