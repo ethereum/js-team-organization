68edcfe4570b04da349e64e7b5db026daae1de7b
Marc Garreau <marcdgarreau@gmail.com>
2019-12-19 13:04:03 -0700

    docs: welcome page (#14)

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,6 @@
 export default {
   title: 'EF JavaScript Team',
   description: 'A guide to projects maintained by the EF JS team',
-  files: './website/**/*.{md,mdx}'
+  files: './website/**/*.{md,mdx}',
+  menu: ['Welcome', 'Projects']
 }
